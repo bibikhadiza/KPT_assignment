bd329e6cba104e52bd47cea94082c0041731d6d4
Bibi Khadiza <limakhadiza@yahoo.com>
2017-04-29 23:27:59 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/Navbar'
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <p>dsfhskfjh</p>
+      </div>
+    );
+  }
+}
+
+export default App;
