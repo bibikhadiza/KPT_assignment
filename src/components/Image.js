a54631bb6ff78ce6f370ccd466a8cbd85e12bbab
Bibi Khadiza <limakhadiza@yahoo.com>
2017-04-30 14:47:07 -0400

    finished hero image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 import "../style/Image.css"
-import image from '../15375d0f0cd93f9832916892016291f4b78bf1c1_billboard-prep-options-c (2).jpg'
+
 
 
 const HeroImage = () => (
   <div className="heroImage">
-    <Image src={image} fluid={true} />
+    <div className="heroImage-content">
+    <h1>
+      Choose your SAT <sup>®</sup> Prep
+    </h1>
+    <hr></hr>
+    <h4>Proven strategies. Personalized prep.</h4>
+  </div>
   </div>
 )
 
